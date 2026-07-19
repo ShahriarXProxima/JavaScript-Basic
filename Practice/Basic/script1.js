c15a5aaaabcad473d2d91165b459350f33f531c6
ShahriarXProxima <shahriarxproximalog1@gmail.com>
2026-07-19 21:26:51 +0600

    Bsic JavaScript practice

---
+++ Practice/Basic/script1.js
@@ -0,0 +1,6 @@
+let message = document.getElementById("message");
+let btn = document.getElementById("changeBtn");
+
+btn.addEventListener("click", function () {
+    message.innerHTML = "you have clicked!"
+})
